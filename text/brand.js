4bad689da564770ed8319d64ab740e5b12ad8598
juanchos2018 <jcarlossenati@gmail.com>
2021-10-16 19:37:09 -0500

    fit

---
+++ text/brand.js
@@ -0,0 +1,16 @@
+const brand = {
+  movie: {
+    name: 'Movie',
+    desc: 'Veluxi Movie - Vue Single Landing Page Template',
+    prefix: 'veluxi',
+    footerText: 'Veluxi Theme 2020',
+    logoText: 'Veluxi Movie',
+    projectName: 'Movie',
+    url: 'veluxi.ux-maestro.com/movie',
+    img: '/static/images/movie-logo.png',
+    notifMsg:
+      'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+}
+
+export default brand
---
+++ text/link.js
@@ -0,0 +1,11 @@
+const link = {
+    movie: {
+        home: '/',
+        detail: '/detail-movie',
+        login: '/login',
+        register: '/register',
+
+    }
+}
+
+export default link
